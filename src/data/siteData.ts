7f0529272e4682c61b7b858685e98a5c653c7204
Mouayad <muayad.perun@outlook.com>
2025-09-02 16:54:26 +0300

    feat: update info in `siteData`

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,28 +1,29 @@
 
 
 export const SITE_DATA = {
+    navTitle: "MHM",
     seo: {
-        siteTitle: "ONE",
-        homeTitle: "Portfolio",
-        description: 'Minimal personal website built with Astro.',
+        siteTitle: "Mouayad Alhamwi",
+        homeTitle: "Personal website",
+        description: "Welcome to my corner of web. My name is Mouayad Alhamwi, I'm a Muslim Software Engineer focused on Flutter.",
     },
     home: {
         about: {
-            name: "ONE",
+            name: "Mouayad Alhamwi",
         },
         contactMe: {
-            email: 'me@example.com',
+            email: 'mouayad.alhamwi.ma@gmail.com',
             accounts: [
                 {
                     title: 'Github',
                     icon: "carbon:logo-github",
-                    url: 'https://github.com/',
+                    url: 'https://github.com/dmouayad',
                     buttonLabel: "View my GitHub account"
                 },
                 {
                     title: 'LinkedIn',
                     icon: "carbon:logo-linkedin",
-                    url: 'https://www.linkedin.com/',
+                    url: 'https://www.linkedin.com/in/mouayad-alhamwi-85bb9420b/',
                     buttonLabel: "Connect with me on LinkedIn"
                 },
             ]
